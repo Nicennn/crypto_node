c0543b1de355afe7222ee730daeed5a6357e7b34
ancap <mathiaspeterhorst@gmail.com>
2018-07-08 20:04:24 +0200

    some progress

--- routes/router.js
+++ routes/router.js
@@ -32,11 +32,32 @@ module.exports = (app, currencies) => {
 				console.log("status code: ", res.statusCode); 
 				console.log("headers: ", res.headers);
 
+				// Reconstitute the body from multiple data chunks
 				res.on("data", data =>  { body += data });
 
+				// Data has been consumed
 				res.on("end", () => {
 					requestResult = JSON.parse(body);
-					console.log(requestResult);
+					let data = requestResult.data;
+					console.log(data);
+					console.log("\n##############END DATA#############\n");
+
+
+					for (var key in data) {
+						if (data.hasOwnProperty(key)) {
+							let coin = data[key];
+							//console.log("requestResult.data[key]: ", typeof(requestResult.data[key] ));
+							//console.log("data[key]: ", typeof(data[key] ));
+							//console.log("coin: ", coin);
+							//console.log("reqestResult: ", typeof(requestResult));
+							//console.log("coin.id: ", coin.id);
+							//currencies.push(Object.assign(requestResult.key));
+						}
+					}
+					console.log(currencies);
+					currencies.forEach(elem => {
+						console.log(elem.name);
+					})
 				});
 			});
 
